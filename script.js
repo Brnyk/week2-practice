d58b2147ab0d4c5d3a2665186a999d06a8f33b4c
Hornyák <bence.hornyak63@gmail.com>
2023-09-20 19:52:48 +0200

    conditionals, objects, functions

--- script.js
+++ script.js
@@ -1,28 +1,93 @@
-function logger(text) {
+/* let stringVariable = "text"
+let numberVariable = 15;
+let booleanVariable = true;
+
+function myFunc (param1, param2){
+    return param1 + param2;
+}
+
+console.log(stringVariable, numberVariable, booleanVariable) */
+
+/* let array = ["alma", 12, false, undefined, null]
+
+console.log(array) */
+
+/* let stringArray = ["Imola Ács", "Pali Herrer", "Nagy Zsombor", "Bokor Ágota"] 
+
+console.log(stringArray[0]) */
+
+/* let myObj = {
+    key1: "value1",
+    key2: "value2",
+    key3: "value3",
+    key4: 15,
+    key5: 600,
+    key6: true,
+    key7: ["Imola Ács", "Pali Herrer", "Nagy Zsombor", "Bokor Ágota"]
+}
+
+console.log(myObj.key3) */
+
+/* let myFacebook = {
+  name: "Hornyák Bence",
+  age: "25",
+  location: "Miskolc",
+  friends: ["Gipsz Jakab", "John Doe", "Példa Péter"],
+  favoriteMovies: [
+    {
+      title: "Superbad",
+      year: 2008,
+      characters: ["McLovin", "Micheal Cera", "Jonah Hill"],
+    },
+    {
+      title: "Ananász Expressz",
+      year: 2012,
+      characters: ["Csoki", "Gaben", "Sanyi"],
+    },
+    {},
+  ],
+};
+
+console.log(myFacebook.favoriteMovies[0].characters[1]); */   /* Házi -> Tömböt gyártani, minél kombináltabbat, komolyabbat, peacesebbet -> Ital kiválasztó? Minőségileg tömbösíteni?*/
+
+
+/* Objektum: {} Összefüggnek,összetartoznak , Tömb []*/
+
+function logger(text){
     console.log(text)
 }
 
+function negativeOrPositive(number){
+    if (number < 0) {
+        return "negative"
+    } else if (number > 0) {
+        return "positive"
+    } else {
+        return "zero"
+    }
+}
+// logger(negativeOrPositive())
 
-/* logger('hello')
-logger('ciao')
-logger('szia') */
-
-logger('this is my first logger function')
-
-function logTheSumTwoNumbers(x,y) { /* camelCase */
-/*  let x = 1
-    let y = 2 */
-
-    console.log(x+y);
+function decideIfBeerIsGood(beer){
+    if(beer.price < 340 && beer.abv > 4.7) {
+    return beer.name + ": jó sör mert olcsó és üt"
+    } else if (beer.price < 340) {
+        return beer.name + ": jó mert olcsó"
+    } else if (beer.abv > 4.7) {
+        return beer.name + ": jó sör mert üt"
+    } else {
+        return beer.name + ": nem jó"
+    }
 }
 
-/* logTheSumTwoNumbers(1,2); */
+let dreher = { name:"Dreher", price: 349, abv: 5}
+let soproni = { name:"Soproni", price: 329, abv: 4.5}
+let borsodi = {name:"Borsodi", price: 339, abv: 4.8}
+let pecsi = { name:"Pécsi", price: 400, abv: 3.5}
 
-function sumOfTwoNumbers(x,y) {
-    let sum = 0;
-    sum = x + y;
 
-    return sum;
-}
 
-logger(sumOfTwoNumbers(10, 5))
+logger(decideIfBeerIsGood(dreher))
+logger(decideIfBeerIsGood(soproni))
+logger(decideIfBeerIsGood(borsodi))
+logger(decideIfBeerIsGood(pecsi))
